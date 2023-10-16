57c845e9bfe8589dafa958448c0aa0e37a5d9f96
Scott Sickles <scottsickles@gmail.com>
2023-10-16 11:59:59 -0400

    added the path

--- variables.tf
+++ variables.tf
@@ -7,3 +7,10 @@ EOF
   type    = map(string)
   default = {}
 }
+
+variable "path" {
+  type        = string
+  description = <<EOF
+The path to route to this application. Any requests to the API Gateway beginning with this path will be routed to this application.
+EOF
+}
