1ad7c7d0b697aa151d33a28b503bf230af3063d8
Scott Sickles <scottsickles@gmail.com>
2023-10-27 14:43:09 -0400

    pull in the endpoint-types from the api-gateway

--- gateway.tf
+++ gateway.tf
@@ -6,4 +6,5 @@ data "ns_connection" "api-gateway" {
 locals {
   public_urls = [for u in data.ns_connection.api-gateway.outputs.public_urls : { url = u }]
   domain_name = data.ns_connection.api-gateway.outputs.domain_name
+  endpoint_types = data.ns_connection.api-gateway.outputs.endpoint_types
 }
