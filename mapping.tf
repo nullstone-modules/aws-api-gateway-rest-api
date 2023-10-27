25c2d92ae368b60ff9b95caba19433de9e5940b6
Scott Sickles <scottsickles@gmail.com>
2023-10-26 20:34:12 -0400

    added a deployment, stage, and base_path mapping

--- mapping.tf
+++ mapping.tf
@@ -29,7 +29,7 @@ resource "aws_api_gateway_method" "proxy-any" {
   http_method   = "ANY"
 }
 
-resource "aws_api_gateway_integration" "integration" {
+resource "aws_api_gateway_integration" "this" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
   resource_id             = aws_api_gateway_rest_api.this.root_resource_id
   http_method             = aws_api_gateway_method.root-any.http_method
@@ -38,9 +38,39 @@ resource "aws_api_gateway_integration" "integration" {
   uri                     = local.function_arn
 }
 
-resource "aws_apigatewayv2_api_mapping" "this" {
-  api_id          = aws_api_gateway_rest_api.this.id
-  domain_name     = local.domain_name
-  api_mapping_key = var.path
-  stage           = "$default"
+resource "aws_api_gateway_deployment" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_method.root-any.id,
+      aws_api_gateway_resource.proxy.id,
+      aws_api_gateway_method.proxy-any.id,
+      aws_api_gateway_integration.this.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "default" {
+  deployment_id = aws_api_gateway_deployment.this.id
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  stage_name    = "$default"
+}
+
+resource "aws_api_gateway_base_path_mapping" "example" {
+  api_id      = aws_api_gateway_rest_api.this.id
+  stage_name  = aws_api_gateway_stage.default.stage_name
+  domain_name = local.domain_name
+  base_path = var.path
 }
